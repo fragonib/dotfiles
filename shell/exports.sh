64a604bac3310c73a56137b2bad5b71237481d8b
Francisco González Ibáñez <f.gonzalez@lookiero.com>
2021-04-24 17:43:26 +0200

    Clean up

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
+
 export GEM_HOME="$HOME/.gem"
 export GOPATH="$HOME/.go"
 export HOMEBREW_PREFIX="/opt/homebrew";
